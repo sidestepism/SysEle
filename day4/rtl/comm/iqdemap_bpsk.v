d6bcd36290615094968e1e7219ca6ee4bca80dde
Ryohei Fushimi <fushimi.ryohe.i@gmail.com>
2013-12-10 13:55:44 +0900

    update iqdemap simulation

--- day4/rtl/comm/iqdemap_bpsk.v
+++ day4/rtl/comm/iqdemap_bpsk.v
@@ -1,63 +1,60 @@
 
 module iqdemap_bpsk 
-  (
-   input               CLK,
-   input               RST,
+(
+ input               CLK,
+ input               RST,
 
-   input               ce,
+ input               ce,
 
-   input               valid_i,
-   input signed [10:0] ar,
-   input signed [10:0] ai,
+ input               valid_i,
+ input signed [10:0] ar,
+ input signed [10:0] ai,
 
-   output              valid_o,
-   output [127:0]      writer_data,
+ output              valid_o,
+ output [127:0]      writer_data,
 
-   output              valid_raw,
-   output              raw
-   );
+ output              valid_raw,
+ output              raw
+ );
 
-   reg[7:0] bytes;
-   reg[127:0] data;
-   reg valid_output;
+reg[7:0] bytes;
+reg[127:0] data;
+reg valid_output;
 
-   reg[2:0] state;
-   assign valid_o = valid_output;
-   assign valid_raw = 0;
+reg[2:0] state;
 
-    assign writer_data = data;
+assign valid_o = valid_output;
+assign valid_raw = 0;
+assign writer_data = data;
 
-   always @(posedge CLK)
-       if (!RST) begin
-          state <= 0;
-	   bytes <= 0;
-          valid_output <= 0;
-       end else begin
-         if(state == 0) begin
-          valid_output <= 0;
-           if(valid_i) begin
-		state <= 1;
-             bytes <= 0;
-		valid_output <= 0;
-           end
-         end 
-	   
-	   if(state == 1) begin
-
-        //decode
-      data = (data << 1) + (ar > 0);
-	bytes = bytes + 1;
-       if(bytes == 0) begin
-          state <= 2;
-	    end
-
-	
-         end else if(state == 2) begin
-          // output
-	    valid_output <= 1;
-	     state <= 0;
-     end else begin
+always @(posedge CLK) begin
+   if (!RST) begin
+      state <= 0;
+      bytes <= 0;
+      data = 0;
+      valid_output <= 0;
+   end else begin
+      if(state == 0) begin
+         valid_output <= 0;
+         if(valid_i) begin
+            state <= 1;
+            bytes <= 0;
+            valid_output <= 0;
          end
-     
+      end
+      if(state == 1) begin
+        //decode
+        data = (data << 1) + (ar > 0);
+        bytes = bytes + 1;
+        if(bytes == 128) begin
+           state <= 2;
+        end
+      end else if(state == 2) begin
+         // output
+         valid_output <= 1;
+         state <= 0;
+      end else begin
+      end
    end
+end
 endmodule
--- day4/rtl/comm/iqdemap_bpsk_sim.v
+++ day4/rtl/comm/iqdemap_bpsk_sim.v
@@ -4,51 +4,33 @@ module iqdemap_bpsk_sim;
 
    reg ck, rst, ce, valid_i;
    reg signed [10:0] ar, ai;
-   wire valid_o, valid_raw, rawdmodule;
-   wire [127:0] writer_data;
+   output valid_o, valid_raw, rawdmodule;
+   output [127:0] writer_data;
    
-   parameter STEP = 10;
-
-   always begin
-      ck = 0; # (STEP/2)
-      ck = 1; # (STEP/2)
-   end
    reg data[127:0];
-   iqdemap_bpsk(ck, rst, ce, valid_i, ar, ai, valid_o, writer_data, valid_raw, raw);
-   always #10 begin
-      ar = $random % 2 ? 1 : -1;
-      ai = 0;
-      $monitor("%t valid_i: %b, valid_o: %b, (%d, %d)", $time, valid_i, valid_o, ar, ai)
-      if(valid_o) begin
-         $monitor("%b", writer_data);
-      end
-    end
 
    initial begin
+      $dumpfile("testadd.vcd");
+      $dumpvars;
+      ck <= 0;
+      $monitor("%t valid_i: %b, valid_o: %b, (%d, %d)", $time, valid_i, valid_o, ar, ai);
+
       #20 rst <= 0;
       #20 rst <= 1;
-      #20
-      valid_i <= 1;
-      #2560
-      valid_i <= 0;
+      #20 valid_i <= 1;
+      #2560 valid_i <= 0;
+      #100 $finish;
+   end
+   always #10 ck <= ~ck;
+
+   always @(posedge ck) begin
+      ar = $random % 2 ? 1023 : -1024;
+      ai = 0;
+      $monitor("%t valid_i: %b, valid_o: %b, (%d, %d)", $time, valid_i, valid_o, ar, ai);
+      if (valid_o) begin
+         $monitor("%b", writer_data);
+      end
    end
 
-
-
+   iqdemap_bpsk iqdemap_bpsk_ins(ck, rst, ce, valid_i, ar, ai, valid_o, writer_data [127:0], valid_raw, raw);
 endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
