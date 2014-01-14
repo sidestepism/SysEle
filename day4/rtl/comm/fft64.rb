d59a8bf5ee697ec1da7b3216a504ae0c1e87f4e9
Ryohei Fushimi <fushimi.ryohe.i@gmail.com>
2014-01-14 18:48:17 +0900

    pipelined

--- day4/rtl/comm/fft64.rb
+++ day4/rtl/comm/fft64.rb
@@ -20,13 +20,13 @@ class Reg
 
         def wr(i)
             s = cos(i.to_f/32*PI)
-            t = (s * (2**15)).round
+            t = (s * (2**19)).round
             return t;
         end
 
         def wi(i)
             s = -sin(i.to_f/32*PI)
-            t = (s * (2**15)).round
+            t = (s * (2**19)).round
             return t;
         end
         # print "#{wr(n)}, #{wi(n)}\n"
@@ -37,33 +37,6 @@ class Reg
          # "datar[#{d0}] <= datar[#{d0}] - (datar[#{d1}] * #{wr(n)} + datai[#{d1}] * #{wi(n)}) <<< 24;",
          # "datai[#{d0}] <= datai[#{d0}] - (datar[#{d1}] * #{wi(n)} - datai[#{d1}] * #{wr(n)}) <<< 24;"]
     end
-end
-
-64.times do |j|
-    def wr(i)
-        s = cos(i.to_f/32*PI)
-        t = (s * (2**11)).floor
-        return t;
-    end
-
-    def wi(i)
-        s = -sin(i.to_f/32*PI)
-        t = (s * (2**11)).floor
-        return t;
-    end
-    print "#{wr(j)} #{wi(j)}"
-end
-
-
-
-class Car
-  def initialize(carname)
-    @name = carname
-  end
-  
-  def dispName
-    print(@name, "¥n")
-  end
 end
 
 regs = [];
@@ -98,87 +71,108 @@ end
         butterflies += regs[a].butterflyWith(regs [b], n);
         print "#{k}段目の butterfly: #{a} with #{b}, n = #{n}\n"
     end
+    # padding
+    butterflies += [[], [], [], []];
 end
 
 
 6.times do |k|
     print "assign ar#{k} = \n";
     state = 2;
-    32.times do |i|
-        print " state == #{state} ? " + butterflies[i*6+k][0] + " :"
+    44.times do |i|
+        if (butterflies.count <= i*6+k) then break end;
+        print " state == #{state} ? " + butterflies[i*6+k][0] + " :" unless butterflies[i*6+k].empty?
         state += 1;
     end
     print "    0;\n"
 
     print "assign ai#{k} = \n";
     state = 2;
-    32.times do |i|
-        print " state == #{state} ? " + butterflies[i*6+k][1] + " :"
+    44.times do |i|
+        if (butterflies.count <= i*6+k) then break end;
+        print " state == #{state} ? " + butterflies[i*6+k][1] + " :" unless butterflies[i*6+k].empty?
         state += 1;
     end
     print "    0;\n"
 
     print "assign br#{k} = \n";
     state = 2;
-    32.times do |i|
-        print " state == #{state} ? " + butterflies[i*6+k][2] + " :"
+    44.times do |i|
+        if (butterflies.count <= i*6+k) then break end;
+        print " state == #{state} ? " + butterflies[i*6+k][2] + " :" unless butterflies[i*6+k].empty?
         state += 1;
     end
     print "    0;\n"
 
     print "assign bi#{k} = \n";
     state = 2;
-    32.times do |i|
-        print " state == #{state} ? " + butterflies[i*6+k][3] + " :"
+    44.times do |i|
+        if (butterflies.count <= i*6+k) then break end;
+        print " state == #{state} ? " + butterflies[i*6+k][3] + " :" unless butterflies[i*6+k].empty?
         state += 1;
     end
     print "    0;\n"
 
     print "assign wr#{k} = \n";
     state = 2;
-    32.times do |i|
-        print " state == #{state} ? " + butterflies[i*6+k][4] + " :"
+    44.times do |i|
+        if (butterflies.count <= i*6+k) then break end;
+        print " state == #{state} ? " + butterflies[i*6+k][4] + " :" unless butterflies[i*6+k].empty?
         state += 1;
     end
     print "    0;\n"
 
     print "assign wi#{k} = \n";
     state = 2;
-    32.times do |i|
-        print " state == #{state} ? " + butterflies[i*6+k][5] + " :"
+    44.times do |i|
+        if (butterflies.count <= i*6+k) then break end;
+        print " state == #{state} ? " + butterflies[i*6+k][5] + " :" unless butterflies[i*6+k].empty?
         state += 1;
     end
     print "    0;\n"
 end
 
-state = 3;
+state = 4;
 
-32.times do |i|
+42.times do |i|
     print "#{state}: begin\n"
-    print "#{butterflies[i*6+0][0]} <= xr0; "
-    print "#{butterflies[i*6+0][1]} <= xi0; "
-    print "#{butterflies[i*6+0][2]} <= yr0; "
-    print "#{butterflies[i*6+0][3]} <= yi0; \n"
-    print "#{butterflies[i*6+1][0]} <= xr1; "
-    print "#{butterflies[i*6+1][1]} <= xi1; "
-    print "#{butterflies[i*6+1][2]} <= yr1; "
-    print "#{butterflies[i*6+1][3]} <= yi1; \n"
-    print "#{butterflies[i*6+2][0]} <= xr2; "
-    print "#{butterflies[i*6+2][1]} <= xi2; "
-    print "#{butterflies[i*6+2][2]} <= yr2; "
-    print "#{butterflies[i*6+2][3]} <= yi2; \n"
-    print "#{butterflies[i*6+3][0]} <= xr3; "
-    print "#{butterflies[i*6+3][1]} <= xi3; "
-    print "#{butterflies[i*6+3][2]} <= yr3; "
-    print "#{butterflies[i*6+3][3]} <= yi3; \n"
-    print "#{butterflies[i*6+4][0]} <= xr4; "
-    print "#{butterflies[i*6+4][1]} <= xi4; "
-    print "#{butterflies[i*6+4][2]} <= yr4; "
-    print "#{butterflies[i*6+4][3]} <= yi4; \n"
-    print "#{butterflies[i*6+5][0]} <= xr5; "
-    print "#{butterflies[i*6+5][1]} <= xi5; "
-    print "#{butterflies[i*6+5][2]} <= yr5; "
-    print "#{butterflies[i*6+5][3]} <= yi5; "
+    if (butterflies.count <= i*6) then break end;    
+    unless butterflies[i*6+0].empty?
+        print "#{butterflies[i*6+0][0]} <= xr0; "
+        print "#{butterflies[i*6+0][1]} <= xi0; "
+        print "#{butterflies[i*6+0][2]} <= yr0; "
+        print "#{butterflies[i*6+0][3]} <= yi0; \n"
+    end
+    unless butterflies[i*6+1].empty?
+        print "#{butterflies[i*6+1][0]} <= xr1; "
+        print "#{butterflies[i*6+1][1]} <= xi1; "
+        print "#{butterflies[i*6+1][2]} <= yr1; "
+        print "#{butterflies[i*6+1][3]} <= yi1; \n"
+    end
+    unless butterflies[i*6+2].empty?
+        print "#{butterflies[i*6+2][0]} <= xr2; "
+        print "#{butterflies[i*6+2][1]} <= xi2; "
+        print "#{butterflies[i*6+2][2]} <= yr2; "
+        print "#{butterflies[i*6+2][3]} <= yi2; \n"
+    end
+    unless butterflies[i*6+3].empty?
+        print "#{butterflies[i*6+3][0]} <= xr3; "
+        print "#{butterflies[i*6+3][1]} <= xi3; "
+        print "#{butterflies[i*6+3][2]} <= yr3; "
+        print "#{butterflies[i*6+3][3]} <= yi3; \n"
+    end
+    unless butterflies[i*6+4].empty?
+        print "#{butterflies[i*6+4][0]} <= xr4; "
+        print "#{butterflies[i*6+4][1]} <= xi4; "
+        print "#{butterflies[i*6+4][2]} <= yr4; "
+        print "#{butterflies[i*6+4][3]} <= yi4; \n"
+    end
+    unless butterflies[i*6+5].empty?
+        print "#{butterflies[i*6+5][0]} <= xr5; "
+        print "#{butterflies[i*6+5][1]} <= xi5; "
+        print "#{butterflies[i*6+5][2]} <= yr5; "
+        print "#{butterflies[i*6+5][3]} <= yi5; "
+    end
     state += 1;
     print "\n    state <= #{state};\nend\n"
 end
